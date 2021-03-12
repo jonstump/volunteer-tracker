2713ddcfa59ff8ca88957e47d1754670044ab6b2
Jon Stump <jmstump@gmail.com>
2021-03-12 09:19:30 -0800

    pass test for Project.all spec

--- lib/project.rb
+++ lib/project.rb
@@ -8,6 +8,13 @@ class Project
   end
 
   def self.all
-    
+   projects = []
+    returned_projects = DB.exec("SELECT * FROM projects;")
+    returned_projects.each do |project|
+      title = project.fetch('title')
+      id = project.fetch('id').to_i
+      projects << Project.new({titl: title,  id: id})
+    end
+    projects  
   end
 end
