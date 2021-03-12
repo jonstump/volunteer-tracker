b379937cc80eb23e4b20bde76a1b505b12e3a91e
Jon Stump <jmstump@gmail.com>
2021-03-12 13:48:57 -0800

    pass first integration test to add project.

---
+++ app.rb
@@ -0,0 +1,30 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/volunteer')
+require('./lib/project')
+require('pry')
+require('pg')
+also_reload('lib/**/*.rb')
+DB = PG.connect({:dbname => "volunteer_tracker"})
+
+
+
+#==========
+#Projects
+#==========
+get('/') do
+  @projects = Project.all
+  erb(:projects)
+end
+
+get('/projects') do
+  @projects = Project.all
+  erb(:projects)
+end
+
+post('/projects') do
+  project = Project.new({:title => params[:title], :id => nil})
+  project.save
+  @projects = Project.all
+  erb(:projects)
+end
