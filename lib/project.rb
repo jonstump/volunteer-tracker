befd2f696576d3f0520ee16945e62ae234de2827
Jon Stump <jmstump@gmail.com>
2021-03-12 11:39:06 -0800

    add all passing tests for volunteers

--- lib/project.rb
+++ lib/project.rb
@@ -52,6 +52,14 @@ class Project
   end
   
   def volunteers
-
+    volunteers = []
+    returned_volunteers = DB.exec("SELECT * FROM volunteers WHERE project_id = '#{@id}';")
+    returned_volunteers.each() do |volunteer|
+      name = volunteer.fetch('name')
+      project_id = volunteer.fetch('project_id')
+      id = volunteer.fetch('id')
+      volunteers << Volunteer.new({name: name, project_id: project_id, id: id})
+    end
+    volunteers
   end
 end
